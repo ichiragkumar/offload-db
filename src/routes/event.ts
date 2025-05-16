8d3173d5c1acec0954d0d15f2dba6a81c59c2af4
ichiragkumar <chiragroy3030@gmail.com>
2025-05-17 01:18:15 +0530

    minor changes

--- src/routes/event.ts
+++ src/routes/event.ts
@@ -1,18 +1,27 @@
-import express from 'express';
-import { redis, STREAM_KEY } from '../queue/redis';
+import express from "express";
+import { redis, STREAM_KEY } from "../queue/redis";
 
 const router = express.Router();
 
-router.post('/track_event', async (req, res):Promise<any> => {
+router.post("/track_event", async (req, res): Promise<any> => {
   const { user_id, event, timestamp } = req.body;
 
   if (!user_id || !event) {
-    return res.status(400).json({ error: 'Missing user_id or event' });
+    return res.status(400).json({ error: "Missing user_id or event" });
   }
 
-  await redis.xadd(STREAM_KEY, '*', 'user_id', user_id, 'event', event, 'timestamp', timestamp || new Date().toISOString());
+  await redis.xadd(
+    STREAM_KEY,
+    "*",
+    "user_id",
+    user_id,
+    "event",
+    event,
+    "timestamp",
+    timestamp || new Date().toISOString()
+  );
 
-  return res.status(200).json({ message: 'Event received' });
+  return res.status(200).json({ message: "Event received" });
 });
 
 export default router;
